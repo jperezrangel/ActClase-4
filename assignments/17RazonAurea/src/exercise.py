b67403a632d08723d99babc7b85dffdc499d045d
huhwon2379 <88689754+huhwon2379@users.noreply.github.com>
2021-08-23 13:59:53 -0500

    Update exercise.py

--- assignments/17RazonAurea/src/exercise.py
+++ assignments/17RazonAurea/src/exercise.py
@@ -1,7 +1,16 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
+    
+    varphi = (1 + math.sqrt(5)) / 2
+    
+    numero = float(input("Número: "))
+    decimales = int(input("Decimales a mostrar: "))
+    
+    print("Razón áurea:", round(varphi * numero, decimales))
+    
     pass
-
+    
 if __name__ == '__main__':
     main()
+
